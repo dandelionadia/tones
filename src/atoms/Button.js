7bcfbbbfb320c96f363a8889ab1d0bda8dffc628
dandelionadia <nadia.ridko@gmail.com>
2019-07-28 12:00:50 +0200

    adds theme

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -1,15 +1,16 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Button = styled.button`
   padding: 1rem 3rem;
   margin: 15px 0;
   text-transform: uppercase;
   font-size: 11px;
-  background-color: ${({ kind }) =>
-    kind === "primary" ? "#1ed760" : "transparent"};
+  background-color: ${({ kind, theme }) =>
+    kind === 'primary' ? theme.colors.green : theme.colors.transparent};
   border-radius: 2rem;
-  border: ${({ kind }) => (kind === "primary" ? 0 : "2px solid #fff")};
-  color: ${({ kind }) => (kind === "primary" ? "white" : "#1ed760")};
-`;
+  border: ${({ kind }) => (kind === 'primary' ? 0 : '2px solid #fff')};
+  color: ${({ kind, theme }) =>
+    kind === 'primary' ? theme.colors.white : theme.colors.green};
+`
 
-export default Button;
+export default Button
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    green: '#1ed760',
+    white: '#fff',
+    transparent: 'transparent',
+    grey: '#282828',
+    greyLight: '#b3b3b3',
+    greyDark: '#111111',
+    black: '#050505'
+  }
+}

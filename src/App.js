c44715bf56cc331851507a2f2fd9586684ccb424
dandelionadia <nadia.ridko@gmail.com>
2019-07-28 12:10:50 +0200

    cline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Button from './atoms/Button'
 import Heading from './atoms/Heading'
@@ -17,18 +16,6 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Button>press me foo</Button>
         <Button kind="primary">primary</Button>
 

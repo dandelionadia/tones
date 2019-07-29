a7ec6fbbe466df816683bdadb6bd3ad3f9efcec5
dandelionadia <nadia.ridko@gmail.com>
2019-07-29 10:14:27 +0200

    adds styled header

--- src/atoms/Heading.js
+++ src/atoms/Heading.js
@@ -2,12 +2,15 @@ import styled from 'styled-components'
 
 const Heading = styled.h2`
   font-size: 2.5rem; //36
+  position: relative;
+  z-index: 2;
 
   ${props =>
     props.as === `h1` &&
     `
       font-size: 4.6rem; //72
       margin: 0;
+      position: relative;
 		`}
 
   ${props =>
--- src/atoms/Text.js
+++ src/atoms/Text.js
@@ -2,12 +2,22 @@ import styled from 'styled-components'
 
 const Text = styled.p`
   font-size: 1rem; //14
+  position: relative;
+  z-index: 2;
 
   ${props =>
     props.small &&
     `
-			font-size: 0.8rem; //12
-		`}
+			font-size: 0.7rem; //12
+    `}
+
+  ${props =>
+    props.colorGrey &&
+    `
+      color: ${props.theme.colors.greyLight}
+    `}
+  
+  ${props => props.letterSpacing && ` letter-spacing:  0.16rem`}
 `
 
 export default Text

75a021ecc8a4e6354f8563f2fcec783649eda540
dandelionadia <nadia.ridko@gmail.com>
2019-07-29 10:14:27 +0200

    adds styled header

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-  padding: 1rem 3rem;
+  padding: 0.9rem 2.8rem;
   margin: 15px 0;
   text-transform: uppercase;
-  font-size: 11px;
+  font-size: 0.7rem;
   background-color: ${({ kind, theme }) =>
-    kind === 'primary' ? theme.colors.green : theme.colors.transparent};
+    kind === 'primary' ? theme.colors.green : 'rgba(24, 24, 24, 0.7)'};
   border-radius: 2rem;
-  border: ${({ kind }) => (kind === 'primary' ? 0 : '2px solid #fff')};
+  border: none;
+  box-shadow: ${({ kind }) =>
+    kind === 'primary' ? 0 : 'inset 0 0 0 2px #b3b3b3'}
   color: ${({ kind, theme }) =>
     kind === 'primary' ? theme.colors.white : theme.colors.green};
+  position: relative;
+  z-index: 2;
 `
 
 export default Button
--- src/molecules/Header.js
+++ src/molecules/Header.js
@@ -8,17 +8,49 @@ import Button from '../atoms/Button'
 const StyledHeader = styled.div`
   background-color: ${({ theme }) => theme.colors.greyDark};
   padding: 5rem 0 1rem;
+  background-image: url('https://i.scdn.co/image/e359e1b7437818b070ed7578770bdff51513b185');
+  background-size: cover;
+  background-position: 50%;
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: '';
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    position: absolute;
+    z-index: 1;
+    background: linear-gradient(transparent -30%, #181818);
+  }
 `
 
 const Header = () => (
   <StyledHeader>
-    <Text>subtitle</Text>
+    <Text small colorGrey letterSpacing>
+      16,686,235 MONTHLY LISTENERS
+    </Text>
     <Heading as="h1">Linkin Park</Heading>
-    <Composition inline gap={1} templateCols="repeat(2, auto)" padding={1.1}>
-      <Button kind="primary">primary</Button>
-      <Button>press me foo</Button>
+    <Composition
+      inline
+      gap={1}
+      templateCols="repeat(2, auto)"
+      paddingTop={1.1}
+      paddingBottom={2.5}
+    >
+      <Button kind="primary">
+        <Text as="span" small LetterSpacing>
+          play
+        </Text>
+      </Button>
+      <Button>
+        <Text as="span" small letterSpacing>
+          follow
+        </Text>
+      </Button>
     </Composition>
-    <Box flex justifyContent="center">
+    <Box flex justifyContent="center" padding={1.5}>
       menu
     </Box>
   </StyledHeader>

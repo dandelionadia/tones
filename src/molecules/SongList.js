563d975ea9325ce07bc6bafa32325175e119bd19
dandelionadia <nadia.ridko@gmail.com>
2019-08-12 20:25:44 +0200

    uses song list from backend and correcter times milliseconds in the minutes

--- src/molecules/SongList.js
+++ src/molecules/SongList.js
@@ -4,13 +4,24 @@ import Song from './Song'
 
 const SongList = props => (
   <Composition gap="6px">
-    {props.list.map(song => (
-      <Song
-        imageUrl={song.imageUrl}
-        title={song.title}
-        duration={song.duration}
-      />
-    ))}
+    {props.list.slice(0, 5).map(song => {
+      const date = new Date(song.duration_ms)
+      const duration =
+        date.getMinutes() +
+        ':' +
+        date
+          .getSeconds()
+          .toString()
+          .padStart(2, '0')
+
+      return (
+        <Song
+          imageUrl={song.album.images[2].url}
+          title={song.name}
+          duration={duration}
+        />
+      )
+    })}
   </Composition>
 )
 

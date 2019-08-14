695e68c539722ba5d61884936767541a22352db9
dandelionadia <nadia.ridko@gmail.com>
2019-08-14 12:45:09 +0200

    uses artists img and name from local data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,17 +5,23 @@ import ArtistList from './components/ArtistList'
 
 const artistLink = [
   {
-    name: 'Linkin Park'
+    name: 'Linkin Park',
+    id:
+      'https://spotify-proxy-57097.herokuapp.com/v1/artists/6XyY86QOPPrYVGvF9ch6wz',
+    src: 'https://i.scdn.co/image/1685533969d5b68cbc630f991e873bd6467f1814'
   },
   {
-    name: 'Of Monsters and Men'
+    name: 'Of Monsters and Men',
+    id:
+      'https://spotify-proxy-57097.herokuapp.com/v1/artists/4dwdTW1Lfiq0cM8nBAqIIz',
+    src: 'https://i.scdn.co/image/3e38d07dccf32d6b32c1b7768263041ebe5e50c5'
   }
 ]
 
 const Home = () => {
   return (
     <Grid>
-      <Heading>Artist</Heading>
+      <Heading marginTop>Artists</Heading>
       <ArtistList data={artistLink} />
     </Grid>
   )
--- src/pages/home/atoms/Artist.js
+++ src/pages/home/atoms/Artist.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Composition } from 'atomic-layout'
 import styled from 'styled-components'
 import Text from '../../../atoms/Text'
+import { Link } from 'react-router-dom'
 
 const SryledImageContainer = styled.div`
   width: 100%;
@@ -14,16 +15,18 @@ const areasMobile = `
 	text
 `
 
-const Artist = ({ name }) => {
+const Artist = ({ name, artistId, src }) => {
   return (
     <Composition areas={areasMobile}>
       {Areas => (
-        <>
+        <Link to={artistId}>
           <Areas.Img as={SryledImageContainer}>
-            <img src="null" alt="img" />
+            <img src={src} alt="img" />
           </Areas.Img>
-          <Areas.Text as={Text}>{name}</Areas.Text>
-        </>
+          <Areas.Text as={Text} margin tetxCenter colorWhite>
+            {name}
+          </Areas.Text>
+        </Link>
       )}
     </Composition>
   )

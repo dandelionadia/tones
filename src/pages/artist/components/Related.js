263ee7a3bd93822ee9da8fe0daebbfd59aced19a
dandelionadia <nadia.ridko@gmail.com>
2019-08-15 15:52:01 +0200

    adds related part and uses related artists from backend

--- src/pages/artist/components/Related.js
+++ src/pages/artist/components/Related.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Artist from '../../home/atoms/Artist'
+import { Composition } from 'atomic-layout'
+import styled from 'styled-components'
+import Grid from '../../../atoms/Grid'
 
-function Related() {
+const StyledArtistList = styled.div`
+  margin: 2rem 0;
+`
+
+function Related({ match }) {
+  const [relatedArtist, setRelatedArtist] = useState(null)
+  const { artistId } = match.params
+
+  useEffect(() => {
+    fetch(
+      `https://spotify-proxy-57097.herokuapp.com/v1/artists/${artistId}/related-artists`
+    )
+      .then(response => response.json())
+      .then(json => {
+        setRelatedArtist(json)
+      })
+  }, [artistId])
+
+  if (!relatedArtist) {
+    return <p>Loading...</p>
+  }
+  console.log(relatedArtist)
+
   return (
-    <div>
-      <p>Related</p>
-    </div>
+    <Grid>
+      <Composition
+        templateCols="repeat(auto-fill, minmax(160px, 1fr))"
+        gap={2}
+        as={StyledArtistList}
+      >
+        {relatedArtist.artists.map(artistData => (
+          <Artist
+            name={artistData.name}
+            artistId={artistData.id}
+            src={artistData.images[0].url}
+            key={artistData.id}
+          />
+        ))}
+      </Composition>
+    </Grid>
   )
 }
 

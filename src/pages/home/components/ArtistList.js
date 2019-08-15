d7aa46f548c222a068e8412092d892d4414e63a3
dandelionadia <nadia.ridko@gmail.com>
2019-08-15 10:05:23 +0200

    corrected errors

--- src/pages/home/components/ArtistList.js
+++ src/pages/home/components/ArtistList.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Composition } from 'atomic-layout'
+import styled from 'styled-components'
 import Artist from '../atoms/Artist'
+
+const StyledArtistList = styled.div`
+  margin: 2rem 0;
+`
 
 const ArtistList = ({ data }) => {
   return (
-    <Composition templateCols="repeat(auto-fill, minmax(160px, 1fr))" gap={2}>
+    <Composition
+      templateCols="repeat(auto-fill, minmax(160px, 1fr))"
+      gap={2}
+      as={StyledArtistList}
+    >
       {data.map(item => (
-        <Artist name={item.name} artistId={item.id} src={item.src} />
+        <Artist
+          name={item.name}
+          artistId={item.id}
+          src={item.src}
+          key={item.id}
+        />
       ))}
     </Composition>
   )

c2f7ec4173b7ac3b5d0072bb84ae72fd78582a87
dandelionadia <nadia.ridko@gmail.com>
2019-08-23 15:24:01 +0200

    made naw sticky

--- src/molecules/NawBar.js
+++ src/molecules/NawBar.js
@@ -10,7 +10,11 @@ const areasMobile = `
 
 const StyledNavBar = styled.div`
   background-color: ${({ theme }) => theme.colors.blackDark};
-  height: auto;
+  height: 100vh;
+  padding: 2rem;
+  box-sizing: border-box;
+  position: sticky;
+  top: 0;
 `
 
 const NawBar = () => {
@@ -18,17 +22,15 @@ const NawBar = () => {
     <Composition areas={areasMobile} as={StyledNavBar} gap={2}>
       {Areas => (
         <>
-          <div>
-            <Areas.Logo>
-              <div>Logo</div>
-            </Areas.Logo>
-            <Areas.Links>
-              <div>Links</div>
-            </Areas.Links>
-            <Areas.UserInfo flex align="end">
-              <div>user info</div>
-            </Areas.UserInfo>
-          </div>
+          <Areas.Logo>
+            <div>Logo</div>
+          </Areas.Logo>
+          <Areas.Links>
+            <div>Links</div>
+          </Areas.Links>
+          <Areas.UserInfo flex align="end">
+            <div>user info</div>
+          </Areas.UserInfo>
         </>
       )}
     </Composition>
